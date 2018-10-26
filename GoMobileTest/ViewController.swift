e370a8eb28a66a939c0585c7f5bfa74f8136a6e5
nhatlt2 <nhatlt2@vng.com.vn>
2018-10-26 10:05:25 +0700

    Initial Commit

---
+++ GoMobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoMobileTest
+//
+//  Created by Nhat Le on 10/26/18.
+//  Copyright © 2018 Nhat Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
