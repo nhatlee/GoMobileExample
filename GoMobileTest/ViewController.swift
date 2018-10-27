694aff8fe7fa4626fbddc0701cb8f2ec3ce8d199
nhatlt2 <nhatlt2@vng.com.vn>
2018-10-27 12:26:56 +0700

    initial commit

--- GoMobileTest/ViewController.swift
+++ GoMobileTest/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Corelib
 
-class ViewController: UIViewController {
+struct ZPConfig: Codable {
+    let key: String
+    let value: String
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    private lazy var dataBasePath: String = {
+        return Bundle.main.path(forResource: "Configuration", ofType: "db") ?? ""
+    }()
+    
+    private var dataSource = [ZPConfig]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadData()
+    }
+    
+    private func loadData() {
+        let coreLib = CorelibNewZpConfig("http://localhost:8002/keys/", "111111", dataBasePath, nil)
+        do {
+            let json = """
+{"data" : [{"key": "bank"}, {"key": "zp"}, {"key": "enable"}, {"key": "ffff"}]}
+"""
+            let data = try coreLib?.getValueForKey(json)
+            let decoder = JSONDecoder()
+            let configs = try decoder.decode([ZPConfig].self, from: data as! Data)
+            dataSource = configs
+            let first = dataSource[0]
+            print("-------------------------->")
+            print("----->FIRST:\(first.key)--->\(first.value)")
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        } catch let error {
+            print("Error:\(error.localizedDescription)")
+        }
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
+            return UITableViewCell()
+        }
+        let item = dataSource[indexPath.row]
+        let textString = String(format: "Item with key:%@, value: %@", item.key, item.value)
+        cell.textLabel?.text = textString
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
 
 }
 
